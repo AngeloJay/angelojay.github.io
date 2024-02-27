8c8fd2a321ed021ccacadbf0ea96a416a05e70fc
AngeloJay <71909986+AngeloJay@users.noreply.github.com>
2024-02-27 14:33:30 -0800

    Update fancifymytext.js
    Updated JavaScipt so that instead of every word ending with "-Moo" only the last word before a sentence ends does it.

--- fancifymytext.js
+++ fancifymytext.js
@@ -14,15 +14,24 @@ function boldText() {
 }
 
 function normalText() {
+    document.getElementById('textBox').style.textDecoration = "none";
+    document.getElementById('textBox').style.color="black";
     document.getElementById('textBox').style.fontWeight="normal";
 }
 
-function upperCasify(){
+function upperCasify() {
     var textArea = document.getElementById('textBox');
-    document.getElementById('textBox').value = document.getElementById('textBox').value.toUpperCase();
-    var textString = document.getElementById('textBox').value;
-    var parts = textString.split(" ");
-    var textString = parts.join("-Moo ");
-    textArea.value = textString;
+    var textString = textArea.value.toUpperCase();
+    var sentences = textString.split(". ");
+    for (var i = 0; i < sentences.length; i++) {
+        var words = sentences[i].split(" ");
+        var lastWordIndex = words.length - 1;
+        if (words[lastWordIndex].endsWith(".")) {
+            words[lastWordIndex] = words[lastWordIndex].replace(".", "-Moo.");
+        }
+        sentences[i] = words.join(" ");
+    }
+    textArea.value = sentences.join(". ");
 }
+
 
